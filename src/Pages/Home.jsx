54a82f04bcbe9fba54372fd516b891331af1c519
anandmohitdeveloper <mohitdeveloper64@gmail.com>
2024-08-09 14:57:18 +0530

    Solved some minor problems

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <div className="Home">
       <section className="container-fluid mt-15 section_1">
-        <div className="container d-flex align-items-center justify-content-center p-5">
-          <div className="row align-items-center justify-content-center gap-2">
+        <div className="container">
+          <div className="row align-items-center justify-content-xl-between justify-content-lg-center justify-content-center gap-2">
             <div className="col-xl-5  col-lg-8 col-md-12 text-lg-start text-center ">
               <h1 className="title-font fs-55">
                 <span className="text-body-secondary">Bridging the gap</span>{" "}
@@ -17,7 +17,7 @@ const Home = () => {
               </p>
             </div>
             <div className="image_Container col-xl-7 col-lg-6 col-md-12">
-              <img src="public/Home_img_1.webp" />
+              <img src="./Home_img_1.webp" />
             </div>
           </div>
         </div>

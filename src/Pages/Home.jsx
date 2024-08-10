0f61f2c07fb266c116fee9fd1c578e95d455a15e
anandmohitdeveloper <mohitdeveloper64@gmail.com>
2024-08-10 11:12:01 +0530

    Solved some Responsiveness problems

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -34,26 +34,30 @@ const Home = () => {
           />
         </header>
 
-        <div className="container-fluid Loan_Container py-7">
-          <div className="row justify-content-md-center justify-content-center">
-            <div className="image_container d-flex flex-column align-items-center col-xl-7 col-lg-6 col-md-10 col-sm-12 p-5">
-              <img className="loan" src="./LOAN.webp" />
-              <img className="loan2" src="./Loan_img_1.webp" />
+        <div className="container-fluid loan_container py-7">
+          <div className="row justify-content-md-center justify-content-center align-items-center">
+            <div className="  col-xl-7 col-lg-6 col-md-12 col-sm-12 ">
+              <div className="image_container d-flex flex-column align-items-center p-5">
+                <img className="loan" src="./LOAN.webp" />
+                <img className="loan2" src="./Loan_img_1.webp" />
+              </div>
             </div>
-            <div className="text_container mx-sm-4 mx-md-0  text-white d-flex flex-column  align-items-start col-xl-5 col-lg-6 col-md-10 col-sm-10 rounded-start-5 ">
-              <p className="fs-36">Loan</p>
-              <p className="font-inter fs-24">
-                Ipsum has been the industry's standard dummy text ever since the
-                1500s, when an unknown printer took a galley of type and
-                scrambled it to make a type specimen book.
-              </p>
-              <div className="btn-container mt-5">
-                <button className="btn fs-3 fw-medium cus-btn">
-                  Apply for loan{" "}
-                  <span className="mx-3 ">
-                    <img src="./Arrow.png" alt="" />
-                  </span>
-                </button>
+            <div className="  col-xl-5 col-lg-6 col-md-10 col-sm-10  ">
+              <div className="text_container mx-sm-4 mx-md-0  text-white d-flex flex-column justify-content-center align-items-start round">
+                <p className="fs-36">Loan</p>
+                <p className="font-inter fs-24">
+                  Ipsum has been the industry's standard dummy text ever since
+                  the 1500s, when an unknown printer took a galley of type and
+                  scrambled it to make a type specimen book.
+                </p>
+                <div className="btn-container mt-5">
+                  <button className="btn fs-4 fw-bold cus-btn font-poppins">
+                    Apply for loan{" "}
+                    <span className="mx-3 ">
+                      <img src="./Arrow.png" alt="" />
+                    </span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>

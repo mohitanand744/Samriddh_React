b5aab5d58fb374f18c134e4fc58c138547291146
anandmohitdeveloper <mohitdeveloper64@gmail.com>
2024-08-11 20:45:22 +0530

    solved some alignment problems

--- src/components/Footers/Footer.jsx
+++ src/components/Footers/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-5 font-inter">
       <div className="container">
-        <div className="row justify-content-md-center">
+        <div className="row justify-content-md-center justify-content-between">
           <div className=" col-md-8 col-lg-3 col-xl-3">
             <div className="row flex-column gap-8 text-md-center order-md-first  order-sm-last order-last">
               <div className="pera fs-18  text-light-gray mt-sm-5">
@@ -21,97 +21,99 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="col-12 col-md-12 col-lg-9  d-flex justify-content-lg-around  flex-sm-wrap flex-wrap justify-content-sm-center justify-content-center gap-5 order-md-last order-sm-first order-first">
-            <div className="col-4 col-sm-6 col-lg-2">
-              <h1 className="footer-heading">Platform</h1>
-              <ul className="list-unstyled">
-                <li className="mt-4">
-                  <a href="/" className="text-white">
-                    Home
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Get in touch
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    How it Works
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    FAQ
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-4 col-sm-6 col-lg-2 ">
-              <h1 className="footer-heading">About</h1>
-              <ul className="list-unstyled">
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    About Us
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a
-                    href="#"
-                    className="text-white d-flex flex-column align-items-start
+          <div className="col-12 col-md-12 col-lg-9 order-md-last order-sm-first order-first ">
+            <div className="d-flex flex-sm-wrap flex-wrap justify-content-sm-center justify-content-center gap-5  gap-3">
+              <div className=" col-sm-6 col-md-2 col-lg-2">
+                <h1 className="footer-heading">Platform</h1>
+                <ul className="list-unstyled">
+                  <li className="mt-4">
+                    <a href="/" className="text-white">
+                      Home
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Get in touch
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      How it Works
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      FAQ
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" col-sm-6 col-md-2 col-lg-2 ">
+                <h1 className="footer-heading">About</h1>
+                <ul className="list-unstyled">
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      About Us
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a
+                      href="#"
+                      className="text-white d-flex flex-column align-items-start
                   "
-                  >
-                    Careers{" "}
-                    <button className="highlight fs-4">Hiring now</button>
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-4 col-sm-6 col-lg-2">
-              <h1 className="footer-heading">Information</h1>
-              <ul className="list-unstyled">
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Terms of Use
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Refund Policy
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Grievance officer
-                  </a>
-                </li>
-              </ul>
-            </div>
+                    >
+                      Careers{" "}
+                      <button className="highlight fs-4">Hiring now</button>
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Blog
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" col-sm-6 col-md-2 col-lg-2">
+                <h1 className="footer-heading">Information</h1>
+                <ul className="list-unstyled">
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Privacy Policy
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Terms of Use
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Refund Policy
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Grievance officer
+                    </a>
+                  </li>
+                </ul>
+              </div>
 
-            <div className="col-4 col-sm-6 col-lg-2">
-              <h1 className="footer-heading">Tools</h1>
-              <ul className="list-unstyled">
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    EMI Calculator
-                  </a>
-                </li>
-                <li className="mt-4">
-                  <a href="#" className="text-white">
-                    Balance transfer calculator
-                  </a>
-                </li>
-              </ul>
+              <div className=" col-sm-6 col-md-2 col-lg-2">
+                <h1 className="footer-heading">Tools</h1>
+                <ul className="list-unstyled">
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      EMI Calculator
+                    </a>
+                  </li>
+                  <li className="mt-4">
+                    <a href="#" className="text-white">
+                      Balance transfer calculator
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>

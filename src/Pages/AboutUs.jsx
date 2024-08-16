23be4462aff23c4f01b60962f054df45e78ba42b
anandmohitdeveloper <mohitdeveloper64@gmail.com>
2024-08-16 11:35:29 +0530

    solved some Alignment problem.

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -7,7 +7,11 @@ const AboutUs = () => {
     <div className="aboutUs">
       <div className={`${style.section_1} mb-55`}>
         <div className="image-container text-center">
-          <img className="img-fluid" src="./aboutUsImg1.png" alt="" />
+          <img
+            className="img-fluid about-hero-img"
+            src="./aboutUsImg1.png"
+            alt=""
+          />
         </div>
       </div>
     </div>

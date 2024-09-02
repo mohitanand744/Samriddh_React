6c56ad1f1870c39f39fa8f0970b16d7919a6a92b
anandmohitdeveloper <mohitdeveloper64@gmail.com>
2024-09-02 17:34:01 +0530

    added 2 more sections

--- src/Pages/InsurancePages/LifeInsurance.jsx
+++ src/Pages/InsurancePages/LifeInsurance.jsx
@@ -4,13 +4,14 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import CreatingContext from "../../context/ContexAPI";
+import EducationLoanTitle from "../../components/Titles/EducationLoanTitle";
 
 const LifeInsurance = () => {
   const { aspirationsSliderCarouselOptions } = useContext(CreatingContext);
 
   return (
     <div className="liftInsurance">
-      <section className="liftInsuranceSection_1 position-relative">
+      <section className=" liftInsuranceSection_1 px-lg-0 px-md-0 px-4 position-relative">
         <div
           data-aos="flip-right"
           className="position-absolute textContainer p-2"
@@ -114,7 +115,7 @@ const LifeInsurance = () => {
         </div>
       </section>
 
-      <section className="liftInsuranceSection_2 px-lg-0 px-md-0 px-4 bg-dark-blue w-100 position-relative">
+      <section className=" liftInsuranceSection_2 px-lg-0 px-md-0 px-4 bg-dark-blue w-100 position-relative">
         <div className="container py-5">
           <div className="aspirationsSlider ">
             <OwlCarousel
@@ -190,6 +191,119 @@ const LifeInsurance = () => {
             </OwlCarousel>
           </div>
         </div>
+      </section>
+
+      <section className=" lifeInsuranceSection_3 px-lg-0 px-md-0 px-4 my-5">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-7 col-md-6 col-12" data-aos="flip-left">
+              <EducationLoanTitle
+                title={"What is Life Insurance?"}
+                color="black"
+                fontSize="fs-40"
+                ratings={false}
+                pera="Lorem ipsum is a placeholder text commonly used in the design and typesetting industry. 
+It consists of parts of Latin words with no specific meaning and is used as temporary filler content when the actual text is not available or finalized."
+                pera2={
+                  "Lorem ipsum is a placeholder text commonly used in the design and typesetting industry. It consists of parts of Latin words with no specific meaning "
+                }
+              />
+            </div>
+            <div className="col-lg-5 col-md-6 col-12">
+              <div
+                data-aos="flip-right"
+                className="d-flex justify-content-center align-items-end"
+              >
+                <img src="/Life Insurance2.png" alt="" className="img-fluid" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className=" lifeInsuranceSection_4 py-1 px-lg-0 px-md-0 px-4 bg-dark-blue">
+        <EducationLoanTitle
+          title={"Types of Life Insurance Plans"}
+          color="white"
+          fontSize="fs-40"
+          ratings={false}
+          pera="Life insurance plans are characterized by the fact that they pay a benefit on death of the insured. The most popular plans have a fixed period of time for which the policy is in force, and death benefit will be paid."
+          pera2={" "}
+        />
+
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="card rounded-5 " data-aos="flip-left">
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div className="">
+                    <h5 className="card-title fs-20 font-inter fw-semibold">
+                      Term Life insurance plan
+                    </h5>
+
+                    <p className="card-text fs-18 font-inter mt-4">
+                      Term Insurance is the simplest form of life insurance that
+                      only offers financial cover for a specific number of
+                      years.
+                    </p>
+                  </div>
+                  <a href="#" className="card-link fw-bold fs-18 font-inter">
+                    Learn More &#x2B9E;
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="card rounded-5 " data-aos="flip-right">
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div className="">
+                    <h5 className="card-title fs-20 font-inter fw-semibold">
+                      Whole Life insurance plans
+                    </h5>
+
+                    <p className="card-text fs-18 font-inter mt-4">
+                      Provides coverage for the entire lifetime of the
+                      policyholder.
+                    </p>
+                  </div>
+                  <a href="#" className="card-link fw-bold fs-18 font-inter">
+                    Learn More &#x2B9E;
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-12">
+              <div className="card rounded-5 " data-aos="flip-left">
+                <div className="card-body d-flex flex-column justify-content-between">
+                  <div className="">
+                    <h5 className="card-title fs-20 font-inter fw-semibold">
+                      Pension plans
+                    </h5>
+
+                    <p className="card-text fs-18 font-inter mt-4">
+                      Designed to provide financial security during retirement
+                      by offering a regular income.
+                    </p>
+                  </div>
+                  <a href="#" className="card-link fw-bold fs-18 font-inter">
+                    Learn More &#x2B9E;
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className=" lifeInsuranceSection_4 py-1 px-lg-0 px-md-0 px-4 ">
+        <EducationLoanTitle
+          title={"Why Do I Need Life Insurance?"}
+          color="black"
+          fontSize="fs-36"
+          ratings={false}
+          pera="Lorem ipsum is a placeholder text commonly used in the design and typesetting industry. It consists of parts of Latin words with no specific meaning and is used as temporary filler content when the actual text is not available or finalized."
+          pera2={" "}
+        />
       </section>
     </div>
   );
